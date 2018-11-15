d42f8a8a783837f862952cf6f298bfc5a61f3ba9
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2018-11-15 13:40:53 +0100

    Build infrastructure

---
+++ build/Tests/PSSA.tests.ps1
@@ -0,0 +1,23 @@
+﻿param ($PSScriptRoot)
+Describe 'Testing against PowerShell Script Analyzer Rules' {
+    Context 'PSSA Standard Rules' {
+
+        $Files = Get-ChildItem -Path $PSScriptRoot\..\..\deploy -Recurse | where {$_.Extension -eq ".ps1"}
+        foreach ($file in $files) {
+
+            $analysis = Invoke-ScriptAnalyzer -Path $file.FullName
+            $scriptAnalyzerRules = Get-ScriptAnalyzerRule
+
+            forEach ($rule in $scriptAnalyzerRules) {
+                It "File $($File.Name) should pass $rule" {
+                    If ($analysis.RuleName -contains $rule) {
+                        $analysis |
+                            Where RuleName -EQ $rule -outvariable failures |
+                            Out-Default
+                        $failures.Count | Should Be 0
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ build/build.ps1
@@ -0,0 +1,5 @@
+﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+
+Install-Module PSScriptAnalyzer -Scope CurrentUser
+Import-Module PSScriptAnalyzer
